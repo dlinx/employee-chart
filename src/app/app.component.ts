84e360b12980a6670d9cbf52ce2a591631968859
Laxmikant Dange <dl.groups007@gmail.com>
2025-01-05 13:48:19 +0530

    Added Employee grid with pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { EmployeesModule } from './employees/employees.module';
-import { ClarityModule } from "@clr/angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, RouterLinkActive,ClarityModule, EmployeesModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    EmployeesModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from "@angular/core";
-import { EmployeeTableComponent, EmployeeTreeComponent } from "./components";
-import { RouterModule, provideRouter } from "@angular/router";
-import { routes } from "./employees.routes";
+import { NgModule } from '@angular/core';
+import { EmployeeTableComponent, EmployeeTreeComponent } from './components';
+import { RouterModule } from '@angular/router';
+import { routes } from './employees.routes';
+import { CommonModule } from '@angular/common';
+import { ClarityModule, ClrDatagridModule } from '@clr/angular';
 
 @NgModule({
-    declarations:[EmployeeTableComponent,EmployeeTreeComponent],
-    imports:[RouterModule.forChild(routes)]
+  declarations: [EmployeeTableComponent, EmployeeTreeComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    ClarityModule,
+    ClrDatagridModule
+  ],
 })
-export class EmployeesModule {
-
-}
+export class EmployeesModule {}
